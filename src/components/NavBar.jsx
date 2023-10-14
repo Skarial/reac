1dc7361f0ada70d735749a777b06d502bb864b77
CHATELAIN JORDAN <jordan139@hotmail.fr>
2023-10-14 21:03:24 +0200

    1 btn par poké+chacun une clé unique qui est ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,28 @@
-
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function NavBar({ onPrevClick, onNextClick }) {
+function NavBar({ onPrevClick, onNextClick, pokemonList, onPokemonButtonClick, activeIndex }) {
   return (
     <nav>
-      <button onClick={onPrevClick}>Précédent</button>
-      <button onClick={onNextClick}>Suivant</button>
+      <div className='button-container'>
+        {pokemonList.map((pokemon, index) => (
+          <button key={index} onClick={() => onPokemonButtonClick(index)}>
+            {pokemon.name}
+          </button>
+        ))}
+      </div>
+      <div className='button-container'>
+      </div>
     </nav>
   );
 }
+
 NavBar.propTypes = {
   onPrevClick: PropTypes.func.isRequired,
   onNextClick: PropTypes.func.isRequired,
+  pokemonList: PropTypes.array.isRequired,
+  onPokemonButtonClick: PropTypes.func.isRequired,
+  activeIndex: PropTypes.number.isRequired,
 };
 
-
 export default NavBar;
-
-
-
-
-
-
-
-
